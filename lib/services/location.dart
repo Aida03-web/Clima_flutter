1004e367b3fda90fa9b96e972777ef6f711b95b8
Aida03-web <78191483+Aida03-web@users.noreply.github.com>
2022-04-24 18:20:51 +0600

    first commit

---
+++ lib/services/location.dart
@@ -0,0 +1,18 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double latitude;
+  double longitude;
+
+  Future<void> getCurrentLocation() async {
+    try {
+      LocationPermission permission;
+      permission = await Geolocator.requestPermission();
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+      print(position);
+    } catch (e) {
+      print(e);
+    }
+  }
+}
